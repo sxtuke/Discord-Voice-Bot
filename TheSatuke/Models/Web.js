341b16e149bbcfce5dd47b435efe370e2c2e9a00
sxtuke <52047528+sxtuke@users.noreply.github.com>
2026-06-04 19:54:39 +0300

    Delete TheSatuke directory

--- TheSatuke/Models/Web.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const WEBGUARD = Schema({
-    guildID: { type: String, default: "" },
-    userID: { type: String, default: "" },
-    roles: { type: Array, default: [] }
-});
-
-module.exports = model("userRoles", WEBGUARD);
